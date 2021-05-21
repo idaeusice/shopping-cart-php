dbdd97c21f729708e2d20766a578321ce98f9cde
Jeremy Carder <jeremy.carder11@outlook.com>
2021-05-21 14:28:11 -0700

    Improvement to navbar look/feel

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
-<div id='navbar' class='navbar navbar-dark bg-dark'>
-    <div id='navHome'><a href='main.php'>home</a></div>
+<div id='navbar' class='navbar navbar-dark'>
+    <div id='navHome'><a href='main.php'>Home</a></div>
     <div id='navCategories'><a href='#'>categories_dropdown</a></div>
     <div id='navCart' class='material-icons'><a href='cart.php'>add_shopping_cart</a></div>
     <div id='loggedInText'><a href='loginForm.php'><?php echo 'Log In' ?></a></div>
