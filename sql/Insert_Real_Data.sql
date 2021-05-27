912b28d713b8d98fd86096a831d47a28a8ddf6b1
Gino Cracco <gacracco@gmail.com>
2021-05-27 14:36:44 -0700

    Completed part of the real data the will be finalized in the database

---
+++ sql/Insert_Real_Data.sql
@@ -0,0 +1,10 @@
+-- -----------------------------------------------------
+-- Schema daintree_db
+-- -----------------------------------------------------
+
+-- This file inserts the real data into the database
+
+INSERT INTO `daintree_db`.`customer` (`email`, `first_name`, `last_name`, `password`, `admin`) VALUES ('admin@daintree.com', 'Admin', 'Account', 'Admin1234', '1');
+INSERT INTO `daintree_db`.`customer` (`email`, `first_name`, `last_name`, `password`) VALUES ('gacracco@gmail.com', 'Gino', 'Cracco', 'Gino1234');
+INSERT INTO `daintree_db`.`customer` (`email`, `first_name`, `last_name`, `password`) VALUES ('ssonvisen', 'Steven', 'Sonvisen', 'Steven1234');
+INSERT INTO `daintree_db`.`customer` (`email`, `first_name`, `last_name`, `password`) VALUES ('jcarder', 'Jeremy', 'Carder', 'Jeremy1234');
