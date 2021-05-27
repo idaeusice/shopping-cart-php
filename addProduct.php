fdd24f1573871a2b990349f87be20a041f5a8b34
Steven <s-sonvisen@hotmail.com>
2021-05-26 17:01:43 -0700

    completed basic file uploading for addProduct.php. Now allowing users with access to addProducts to upload an image - the file is added to includes/resources/images and the complete filepath is to be inserted to the product table.

--- addProduct.php
+++ addProduct.php
@@ -3,56 +3,123 @@
         include ('header.php');
         include ('menu.php');
     print "</div>";
-    //need to perform a check 
+    $productCategory;
+    $productName;
+    $productPrice;
+    $productStock;
+    $productStock;
+    $productDescription;
+    $file;
+    $uploadOk = 0;
+
     if($_SERVER['QUERY_STRING'] == 'upload'){
-        include ('connection.php');
+        //file upload handling. File needs to be uploaded 
+        //before the rest of the form is submitted.
+        $directory = 'includes/resources/images/';
+        $file = $directory . basename($_FILES["productImage"]["name"]);
+        echo '<div style="margin-top: 100px;">' . $file . '</div>';
+        $uploadOk = 0;
+        $filetype = strtolower(pathinfo($file, PATHINFO_EXTENSION));
 
-        $target = './includes/resources/images/';
-        $file = $target . basename($_FILES['productImage']['name']);
+        if($uploadOk === 1){
+            if (move_uploaded_file($_FILES["productImage"]["tmp_name"], $file)) {
+                echo "<div>" . htmlspecialchars( basename( $_FILES["productImage"]["name"])) . "</div>";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    }
+
+    if($_SERVER['QUERY_STRING'] == 'submit'){
+        //table variables that are set after the user uploads an image.
+        $productCategory = $_POST['productCategory'];
+        $productName = $_POST['productName'];
+        $productPrice = $_POST['productPrice'];
+        $productStock = $_POST['productStock'];
+        $productStock = $_POST['productStock'];
+        $productDescription = $_POST['productDescription'];
+        
         $uploadOk = 1;
-        $filetype = strtolower(pathinfo($file,PATHINFO_EXTENSION));
-
-        $sql = "insert into product
-        (catagory_id, name, price, units, description) 
-        VALUES ((select catagory_id from category where category), '1', 'Shmapple', '300', '50', 'A real cellphone');"; 
-        $result = mysqli_query($dbc, $sql);
-
+        if($uploadOk === 1){
+            include ('connection.php');
+    
+            $sql = "
+insert into product(
+    prod_id,
+    catagory_id, 
+    name, 
+    price, 
+    units, 
+    description, 
+    image) 
+VALUES (
+    (select max(prod_id) + 1 from product p),
+    (select catagory_id from category c where c.name like concat(upper(substring('" . $productCategory . "', 1, 1)),lower(substring('" . $productCategory . "', 2)))), 
+    '" . $productName . "', 
+    '" . $productStock . "', 
+    '" . $productPrice . "', 
+    '" . $productDescription . "', 
+    '" . $file . "');";
+            $result = mysqli_query($dbc, $sql);
+        }
     }
 ?>
 
 <div id='addProduct'>
-    <form class="form-signin" action="?upload" method="post" enctype="multipart/form-data" id='productForm'>
-        <table style='margin: auto;'>    
-            <tr>
-                <td colspan=2 style='text-align: center;'><h3>Submit a New Product</h3></td>
-            </tr>
-            <tr>
-                <td colspan=2></td>
-            </tr>
-            <tr>
-                <td>Select image to upload:</td>
-                <td><input type="file" name="productImage" id="productImage"></td>
-            </tr>
-            <tr>
-                <td>Product Name: </td>
-                <td><input type="text" name="productName"></td>
-            </tr>
-            <tr>
-                <td>Product Price: </td>
-                <td><input type="text" name="productPrice"></td>
-            </tr>
-            <tr>
-                <td>Product Stock: </td>
-                <td><input type="text" name="productStock"></td>
-            </tr>
-            <tr>
-                <td colspan=2 style='text-align:center;'><input type="submit" value="Add Product" name="submit"></td>
-            </tr>
-            <input name=“MAX_FILE_SIZE” value=“30000” hidden>
+
+<?php
+if($_SERVER['QUERY_STRING'] == 'upload'){
+    echo '
+<form class="form-signin" action="?submit" method="post" enctype="multipart/form-data" id="productForm">
+<table style="margin: auto;">
+    <tr>
+        <td colspan=2 style="text-align: center;"><h3>Submit a New Product</h3></td>
+    </tr>
+    <tr>
+        <td>Product Name: </td>
+        <td><input type="text" name="productName"></td>
+    </tr>
+    <tr>
+        <td>Product Price: </td>
+        <td><input type="text" name="productPrice"></td>
+    </tr>
+    <tr>
+        <td>Product Category: </td>
+        <td><input type="text" name="productCategory"></td>
+    </tr>
+    <tr>
+        <td>Product Description: </td>
+        <td><input type="textarea" name="productDescription"></td>
+    </tr>
+    <tr>
+        <td>Product Stock: </td>
+        <td><input type="text" name="productStock"></td>
+    </tr>
+    <tr>
+        <td colspan=2 style="text-align:center;"><input type="submit" value="Add Product" name="submit"></td>
+    </tr>    
+    ';
+} else {
+    echo '
+<form class="form-signin" action="?upload" method="post" enctype="multipart/form-data" id="productForm">
+<table style="margin: auto;">
+        <tr>
+            <td colspan=2 style="text-align: center;"><h3>Submit a New Product</h3></td>
+        </tr>
+        <tr>
+            <td rowspan=2 style="border-right: 2px solid #003366;"><h6>Select image to upload:</h6></td>
+            <td style="padding-left: 5px;">
+                <input type="file" name="productImage" id="productImage"><br><br>
+                <input type="submit" value="Attach File" name="submit">
+                </td>
+        </tr>
+    ';
+}
+?>
+<input name=“MAX_FILE_SIZE” value=“30000” hidden>
         </table>
     </form>
 </div>
-
 <?php 
     include ('footer.php');
 ?>
--- menu.php
+++ menu.php
@@ -1,18 +1,15 @@
 <div id='navbar' class='navbar navbar-dark'>
     <div id='navHome'><a href='main.php'>Home</a></div>
-    <div id='categories'><a href='#' onclick='toggleCategories();'>Categories</a></div>
+    <a><div id='categories' onclick='toggleCategories();' style='cursor: pointer;'>Categories</div></a>
     <?php
-    //show cart when logged in as user, add product when logged in as admin
-    if(isset($_SESSION['admin']) == 1){
-        echo "<div id='navCart'><a href='addProduct.php'>Add Product</a></div>";
-    } else {
-        echo "<div id='navCart'><a href='cart.php'>My Cart</a></div>";
-
-    }
-
+        //show cart when logged in as user, add product when logged in as admin
+        if(isset($_SESSION['admin']) == 1){
+            echo "<div id='navCart'><a href='addProduct.php'>Add Product</a></div>";
+        } else {
+            echo "<div id='navCart'><a href='cart.php'>My Cart</a></div>";
+        }
     ?>
     <div id='loggedInText'>
-
     <?php 
         if(isset($_SESSION['email'])){
             echo "<a href='logout.php'>Log Out</a>";
@@ -20,9 +17,9 @@
             echo "<a href='login.php'>Log In</a>";
         }
     ?>
-    
     </div>
 </div>
+
 <div id='categoriesMenu' style='display: none;'>
     <a href="main.php"><div>All Items</div></a>
     <?php
