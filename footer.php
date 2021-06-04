de0dadb3a161017568548fcde68aafafac71dc10
Jeremy Carder <jeremy.carder11@outlook.com>
2021-06-04 15:34:45 -0700

    Cleaned up footer

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 
-<!-- scroll to top button -->                                                                           
-<button onclick="window.scrollTo({top: 0, behavior: 'smooth'});" id="scrollButton" class="btn btn-primary">
-    <!-- chevron icon from google icons -->
-    <span class="material-icons">expand_less</span>
-</button>
+        <!-- scroll to top button -->                                                                           
+        <button onclick="window.scrollTo({top: 0, behavior: 'smooth'});" id="scrollButton" class="btn btn-primary">
+            <!-- chevron icon from google icons -->
+            <span class="material-icons">expand_less</span>
+        </button>
 
-<div class='container footer'>
-    <footer style='text-align: center;'>
-        <p>&copy; 2021 Daintree - All rights reserved</p>
-    </footer>
-</div>
+        <div class='container footer'>
+            <footer style='text-align: center;'>
+                <p>&copy; 2021 Daintree - All rights reserved</p>
+            </footer>
+        </div>
     </body>
 </html>
