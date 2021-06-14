8194b4d371243d3feb9fbe2683127b6ff53a6b3d
Jeremy Carder <jeremy.carder11@outlook.com>
2021-06-13 20:37:34 -0700

    Fixed scroll button not working.

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
-<?php
-echo"
+
     <!-- scroll to top button -->                                                                           
-    <button onclick='window.scrollTo({top: 0, behavior: 'smooth'});' id='scrollButton' class='btn btn-primary'>
+    <button onclick="window.scrollTo({top: 0, behavior: 'smooth'});" id='scrollButton' class='btn btn-primary'>
         <!-- chevron icon from google icons -->
         <span class='material-icons'>expand_less</span>
     </button>
@@ -9,14 +8,12 @@ echo"
     <div class='container footer'>
         <footer style='text-align: center;'>
             <p>&copy; 2021 Daintree - All rights reserved</p>
-";
-    if(isset($_SESSION['cust_id'])){
-        echo"<a href='privacy.php'>Privacy Statement</a>";
-    }
-echo"
+    <?php
+        if(isset($_SESSION['cust_id'])){
+            echo"<a href='privacy.php'>Privacy Statement</a>";
+        }
+    ?>
         </footer>
     </div>
 </body>
 </html>
-";
-?>
