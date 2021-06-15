c9153251b292aa962a125df929b2ebb4143af6e8
Steven <s-sonvisen@hotmail.com>
2021-06-15 15:31:14 -0700

    added a placeholder checkout page.

---
+++ checkout.php
@@ -0,0 +1,12 @@
+<?php
+include ('header.php');
+include ('menu.php');
+?>
+
+<div class='container' style='margin-top: 220px; text-align:center;'>
+    <button class='btn btn-success' type="button" id="checkoutButton">Checkout</button>
+</div>
+
+<?php
+include ('footer.php');
+?>
